7ed9fbfd1a0b1cf5934e6087410d7d8290b67d51
Veok <p.lelental@gmail.com>
2016-11-27 19:24:23 +0100

    added rep catalog

---
+++ src/main/java/dao/RepositoryCatalog.java
@@ -0,0 +1,57 @@
+package dao;
+
+import dao.mappers.*;
+import dao.repositories.*;
+import dao.uow.IUnitOfWork;
+
+import java.sql.Connection;
+
+/**
+ * @author L on 27.11.2016.
+ */
+public class RepositoryCatalog implements IRepositoryCatalog {
+
+    private Connection connection;
+    private IUnitOfWork uow;
+
+    public RepositoryCatalog(Connection connection, IUnitOfWork uow) {
+        super();
+        this.connection = connection;
+        this.uow = uow;
+    }
+    @Override
+    public IGeneralPlayerStatsRepository playersStats() {
+        return new GeneralPlayerStatsRepository(connection, new GeneralPlayerStatsMapper(), uow);
+    }
+    @Override
+    public IPlayerRepository players() {
+        return new PlayerRepository(connection, new PlayerRepositoryMapper(), uow);
+    }
+
+    @Override
+    public ITeamRepository teams() {
+        return new TeamRepository(connection, new TeamMapper(), uow);
+    }
+
+    @Override
+    public IEnumDictionariesRepository dictionaries() {
+        return new EnumDictionariesRepository(connection, new EnumDictionaryMapper(), uow);
+    }
+
+    @Override
+    public IMatchScoreBoardRepository scoreboards() {
+        return new MatchScoreBoardRepository(connection, new MatchScoreBoardMapper(), uow);
+    }
+
+    @Override
+    public IMatchHistoryRepository history() {
+        return new MatchHistoryRepository(connection, new MatchHistoryMapper(), uow);
+    }
+
+
+
+    @Override
+    public IGeneralTeamStatsRepository teamsStats() {
+        return new GeneralTeamStatsRepository(connection, new GeneralPlayerStatsMapper(), uow);
+    }
+}
--- src/main/java/domain/App.java
+++ src/main/java/domain/App.java
@@ -1,18 +1,39 @@
 package domain;
 
+import dao.RepositoryCatalog;
+import dao.repositories.IRepositoryCatalog;
+import dao.uow.IUnitOfWork;
+import dao.uow.UnitOfWork;
+import domain.model.GeneralPlayerStats;
+import domain.model.Player;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 
 public class App {
     public static void main(String[] args) {
+        String url="jdbc:hsqldb:hsql://localhost/workdb";
+        GeneralPlayerStats generalPlayerStats = new GeneralPlayerStats();
+        Player player = new Player();
+        generalPlayerStats.setKills(10);
+        player.setNick("Veok");
         try {
-            Connection connection =
-                    DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/workdb");
 
 
-        } catch (SQLException e) {
+            Connection connection = DriverManager.getConnection(url);
+            IUnitOfWork uow = new UnitOfWork(connection);
+
+            IRepositoryCatalog catalog = new RepositoryCatalog(connection,uow);
+            catalog.players().add(player);
+            uow.saveChanges();
+            List<Player> playerList = catalog.players().withNick("Veok");
+            for(Player playerFromDb: playerList){
+                System.out.println(playerFromDb.getNick());
+            }
+        }catch (SQLException e){
             e.printStackTrace();
         }
     }
